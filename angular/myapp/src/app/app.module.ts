ad21a5f0cb17a05fee58ffd7d41927c63f723d41
aniruddha <aniruddha.kudalkar@gmail.com>
2018-04-20 09:28:47 +0530

    databinding : property, event and two way

--- angular/myapp/src/app/app.module.ts
+++ angular/myapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { MyJumboComponent } from './my-jumbo/my-jumbo.component';
 import { PartOneComponent } from './my-jumbo/part-one.component';
 import { PartTwoComponent } from './my-jumbo/part-two.component';
 import { DividerComponent } from './my-jumbo/divider.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 
 @NgModule({
@@ -21,10 +23,12 @@ import { DividerComponent } from './my-jumbo/divider.component';
     MyJumboComponent,
     PartOneComponent,
     PartTwoComponent,
-    DividerComponent
+    DividerComponent,
+    DatabindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
